266c6ca70ffe97d6381ebc7dcff93c08f98e5534
Victordlgzx <v.deleon@zubex.com.mx>
2026-06-03 18:08:01 -0600

    Update process.js

--- api/process.js
+++ api/process.js
@@ -44,6 +44,6 @@ export default async function handler(req, res) {
     return res.status(200).json({ success: true, extracted });
 
   } catch(e) {
-    return res.status(500).json({ error: e.message });
+    return res.status(500).json({ error: e.message, stack: e.stack, type: e.constructor.name });
   }
 }
